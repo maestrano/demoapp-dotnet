3917c5fdcdaa29288c4163475879dcac4710b51f
alachaum <arnaud.lachaume@maestrano.com>
2014-11-12 16:05:50 -0800

    deps cleanups

--- MnoDemoApp/App_Start/AuthConfig.cs
+++ MnoDemoApp/App_Start/AuthConfig.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Web.WebPages.OAuth;
 
 namespace MnoDemoApp
 {
@@ -12,20 +11,6 @@ namespace MnoDemoApp
         {
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
-
-            //OAuthWebSecurity.RegisterMicrosoftClient(
-            //    clientId: "",
-            //    clientSecret: "");
-
-            //OAuthWebSecurity.RegisterTwitterClient(
-            //    consumerKey: "",
-            //    consumerSecret: "");
-
-            //OAuthWebSecurity.RegisterFacebookClient(
-            //    appId: "",
-            //    appSecret: "");
-
-            //OAuthWebSecurity.RegisterGoogleClient();
         }
     }
 }
--- MnoDemoApp/App_Start/BundleConfig.cs
+++ MnoDemoApp/App_Start/BundleConfig.cs
@@ -1,14 +1,8 @@
 ﻿using System.Web;
-using System.Web.Optimization;
 
 namespace DemoApp
 {
     public class BundleConfig
     {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
-        public static void RegisterBundles(BundleCollection bundles)
-        {
-
-        }
     }
 }
